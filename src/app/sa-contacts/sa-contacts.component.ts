7d79f8c74777ace894df6324f93053d0ff782348
MoustafaRGB <moustafargbpro@outlook.com>
2022-11-12 13:32:36 +0200

    finish design of About, Contacts pages

--- src/app/sa-contacts/sa-contacts.component.ts
+++ src/app/sa-contacts/sa-contacts.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sa-contacts',
   templateUrl: './sa-contacts.component.html',
-  styleUrls: ['./sa-contacts.component.css']
+  styleUrls: ['./sa-contacts.component.css'],
 })
 export class SaContactsComponent implements OnInit {
+  isInputEmpty: boolean = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  handleInput(e: any) {
+    this.isInputEmpty = e.target.value.length !== 0 ? true : false;
   }
-
 }
